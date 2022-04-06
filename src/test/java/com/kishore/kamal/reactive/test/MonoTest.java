53b064a588a7985f7851c427131055c076455060
kamal <kamal.kishore@walmartlabs.com>
2022-04-06 17:21:57 +0530

    Mono - mono.just(), StepVerifier

--- src/test/java/com/kishore/kamal/reactive/test/MonoTest.java
+++ src/test/java/com/kishore/kamal/reactive/test/MonoTest.java
@@ -3,6 +3,8 @@ package com.kishore.kamal.reactive.test;
 import org.junit.jupiter.api.Test;
 
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
@@ -10,5 +12,20 @@ public class MonoTest {
     @Test
     public void test(){
         log.info("Project is set up");
+    }
+    @Test
+    public void monoSubscriber(){
+        log.info("Execution Starts");
+        String name = "Will";
+        Mono<String> mono = Mono.just(name).log();
+        mono.subscribe();
+
+        log.info("---------");
+
+        StepVerifier.create(mono).expectNext(name).verifyComplete();
+
+
+
+        log.info("Execution Ends");
     }
 }
