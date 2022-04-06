0dc1abdfd1e9f606ef0545ffe1edee872d89c5d1
kamal <kamal.kishore@walmartlabs.com>
2022-04-06 18:43:39 +0530

    Mono - subscribe - consumer , consumer+ error, consumer +error+ onComplete , consumer+error+onComplete+Disposable

--- src/test/java/com/kishore/kamal/reactive/test/MonoTest.java
+++ src/test/java/com/kishore/kamal/reactive/test/MonoTest.java
@@ -1,6 +1,7 @@
 package com.kishore.kamal.reactive.test;
 
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -24,6 +25,79 @@ public class MonoTest {
 
         StepVerifier.create(mono).expectNext(name).verifyComplete();
 
+
+
+        log.info("Execution Ends");
+    }
+
+    @Test
+    public void monoSubscriberWithConsumer(){
+        log.info("Execution Starts");
+        String name = "Will";
+        Mono<String> mono = Mono.just(name).log();
+
+        log.info("---------");
+        mono.subscribe(s -> log.info("value is : {}", s));
+        log.info("---------");
+
+        StepVerifier.create(mono).expectNext(name).verifyComplete();
+
+
+
+        log.info("Execution Ends");
+    }
+
+    @Test
+    public void monoSubscriberWithConsumerWithError(){
+        log.info("Execution Starts");
+        String name = "Will";
+        Mono<String> mono = Mono.just(name)
+                .map(a -> {throw new RuntimeException("Testing");});
+
+        log.info("---------");
+        mono.subscribe(s -> log.info("value is : {}", s), s -> log.error("error occured   ") );
+        log.info("---------");
+
+        StepVerifier.create(mono).expectError(RuntimeException.class).verify();
+
+
+
+        log.info("Execution Ends");
+    }
+    @Test
+    public void monoSubscriberWithConsumerWithErrorComplete(){
+        log.info("Execution Starts");
+        String name = "Will";
+        Mono<String> mono = Mono.just(name)
+                .map(String::toUpperCase);
+
+        log.info("---------");
+        mono.subscribe(s -> log.info("value is : {}", s), Throwable::printStackTrace, () -> log.info("finished"));
+        log.info("---------");
+
+        StepVerifier.create(mono).expectNext(name.toUpperCase()).verifyComplete();
+
+        log.info("Execution Ends");
+    }
+
+    @Test
+    public void monoSubscriberWithConsumerWithErrorCompleteDisposable(){
+        log.info("Execution Starts");
+        String name = "Will";
+        Mono<String> mono = Mono.just(name)
+                .map(String::toUpperCase).log();
+
+        log.info("---------");
+        mono.subscribe(s -> log.info("value is : {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("finished"),
+                Subscription::cancel);
+
+        log.info("---------");
+        mono.subscribe(s -> log.info("value is : {}", s),
+                Throwable::printStackTrace,
+                () -> log.info("finished"),
+                subs -> subs.request(2));
 
 
         log.info("Execution Ends");
