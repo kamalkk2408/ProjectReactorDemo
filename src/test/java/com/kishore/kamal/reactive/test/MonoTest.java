6a95a7cbf14f1ada08bbeb4d11c5797a486c9a9b
kamal <kamal.kishore@walmartlabs.com>
2022-04-06 16:24:36 +0530

    init - Project set up

---
+++ src/test/java/com/kishore/kamal/reactive/test/MonoTest.java
@@ -0,0 +1,14 @@
+package com.kishore.kamal.reactive.test;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test(){
+        log.info("Project is set up");
+    }
+}
